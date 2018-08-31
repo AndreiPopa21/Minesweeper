21292f6eb537f45f47082faa38199eb72aa18858
AndreiPopa21 <andrei.popa2199@gmail.com>
2018-08-31 09:59:41 +0300

    Add doesItHaveEnoughFlags() and numberAccordingToInt() static functions

--- app/src/main/java/com/example/andreipopa/minesweepernew/Rules.java
+++ app/src/main/java/com/example/andreipopa/minesweepernew/Rules.java
@@ -47,18 +47,99 @@ public class Rules {
         return false;
     }
 
-    //function for uncovering w
+    //function for uncovering the tiles
     public static Vector<MiniTileInfo> uncoverSpace(GameManager gm,
                                      int inputType,
+                                     int gameMode,
                                      int xCoord,
-                                     int yCoord){
+                                     int yCoord, int startIcon){
 
         Vector<MiniTileInfo> tileInfoVector= new Vector<MiniTileInfo>();
+
+
+
 
         return tileInfoVector;
 
     }
 
+    public static boolean doesItHaveEnoughFlagsSet(GameManager gm,
+                                                   int startX,
+                                                   int startY,
+                                                   int startIcon,
+                                                   int gameMode){
 
+        int[] xDir= new int[]{0,0,0,0,0,0,0,0};
+        int[] yDir= new int[]{0,0,0,0,0,0,0,0};
+
+        switch (gameMode){
+            case GameMode.CLASSICAL:
+                xDir= new int[]{-1,-1,0,1,1,1,0,-1};
+                yDir= new int[]{0,1,1,1,0,-1,-1,-1};
+                break;
+            case GameMode.KNIGHTPATHS:
+                xDir= new int[]{-2,-1,1,2,2,1,-1,-2};
+                yDir= new int[]{1,2,2,1,-1,-2,-2,-1};
+                break;
+            default:
+                throw new RuntimeException("This game mode is either obsolete or does not exist");
+        }
+
+        int necessaryFlags= numberAccordingToIcon(startIcon);
+        int foundFlags=0;
+
+        for(int i=0;i<8;i++){
+            int nextX= startX+xDir[i];
+            int nextY= startY+yDir[i];
+
+            if(gm.getTheTable().getTileAtPosition(nextX,nextY).getTileIcon()
+                    ==IconAnnotations.FLAG){
+                foundFlags+=1;
+            }
+        }
+
+        if(necessaryFlags==foundFlags){
+            return true;
+        }
+
+        return false;
+    }
+
+    public static int numberAccordingToIcon(int icon){
+
+        int number= -1;
+
+        switch (icon){
+            case IconAnnotations.ONE:
+                number=1;
+                break;
+            case IconAnnotations.TWO:
+                number=2;
+                break;
+            case IconAnnotations.THREE:
+                number=3;
+                break;
+            case IconAnnotations.FOUR:
+                number=4;
+                break;
+            case IconAnnotations.FIVE:
+                number=5;
+                break;
+            case IconAnnotations.SIX:
+                number=6;
+                break;
+            case IconAnnotations.SEVEN:
+                number=7;
+                break;
+            case IconAnnotations.EIGHT:
+                number=8;
+                break;
+            default:
+                throw new RuntimeException("Why did you not send a number tile");
+
+        }
+
+        return -1;
+    }
 
 }
